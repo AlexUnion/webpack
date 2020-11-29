6cc0bcac4ba9443c23b5bed0b9c714dafd827513
Al_chemick <alex2015mig@gmail.com>
2020-11-29 21:45:46 +0200

    'init_commit'

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function createElement() {
+    const element = document.createElement('div');
+    element.innerText = 'Hello from Webpack';
+    return element;
+}
+
+document.body.appendChild(createElement());
