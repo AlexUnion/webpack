7275c0741240aa6325cf607ddb9a2a5b630a3700
Al_chemick <alex2015mig@gmail.com>
2020-11-29 22:18:05 +0200

    'add_sass_loader'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './index.css';
+import './main.scss';
 
 function createElement() {
     const element = document.createElement('div');
+    element.classList.add('greetings');
     element.innerText = 'Hello from Webpack';
     return element;
 }
