1c620d4ccb246113b1ac256423e016c9693fb21f
Al_chemick <alex2015mig@gmail.com>
2020-12-01 03:02:50 +0200

    'add_html_plugin'

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import './index.css';
 import './main.scss';
 
+import Logo from '../assets/programmer.svg';
+
+import printMe from "./print";
+
 function createElement() {
     const element = document.createElement('div');
     element.classList.add('greetings');
     element.innerText = 'Hello from Webpack';
+
+    const myLogo = new Image(50, 50);
+    myLogo.src = Logo;
+    element.insertAdjacentElement('beforeend', myLogo);
+
     return element;
 }
 
 document.body.appendChild(createElement());
+printMe();
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
     output: {
-        filename: "bundle.js",
+        filename: "[name].bundle.js",
         path: path.resolve(__dirname, 'dist')
     },
     module: {
@@ -22,7 +31,11 @@ module.exports = {
                     // Compiles Sass to CSS
                     "sass-loader",
                 ],
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
         ]
     }
 }
